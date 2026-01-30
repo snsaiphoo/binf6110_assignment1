e3e3193b9b71326a6a6beda6d07a4d9a02a30dee
snsaiphoo <sabrinasaiphoo@gmail.com>
2026-01-29 23:17:49 -0500

    Adding singularity for Medaka

--- scripts/buildcontainers.sh
+++ scripts/buildcontainers.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 # This script will contain the commands needed to obtain the .sif files needed to run the pipeline
-# These commands should be ran in the $SCRATCH environment, specifically in the containers directory 
-# The jobs will pull from these directories to run the commands 
+# The code below moves you to the $SCRATCH environment
+# Creates the containers folder to hold the .sif files
+# The main jobs will pull from these directories to run the commands 
+
+cd /scratch/$USER/salmonella
+mkdir containers
+cd containers
+
+# Location: scratch/$USER/salmonella/containers
 
 # NanoPlot version 1.46.2
-# scratch/$USER/salmonella/containers
 
 singularity pull docker://quay.io/biocontainers/nanoplot:1.46.2--pyhdfd78af_0
 
-# Flye version 2.9.6 
-# scratch/$USER/salmonella/containers
+# Flye version 2.9.6
 
 singularity pull docker://quay.io/biocontainers/flye:2.9.6--py311h2de2dd3_0
+
+# Medaka version 2.1.1
+
+singularity pull docker://quay.io/biocontainers/medaka:2.1.1--py311h1d3aea1_0
