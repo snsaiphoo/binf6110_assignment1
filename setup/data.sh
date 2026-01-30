244237b67ddd89646d3548f5ff1a7cce8cf562e2
snsaiphoo <166075080+snsaiphoo@users.noreply.github.com>
2026-01-29 20:23:11 -0500

    Update data.sh

--- setup/data.sh
+++ setup/data.sh
@@ -3,7 +3,7 @@
 # This code is meant to be ran in the raw directory of your $SCRATCH environment
 # This is with the assumption you are using a DRAC cluster
 
-module load sra-toolkit
+module load sra-toolkit/3.0.9
 prefetch SRR32410565
 cd SRR32410565
 fasterq-dump SRR32410565.sra
