d42ffb7914aee3b0a02368ad30928d7f3be19bdb
snsaiphoo <sabrinasaiphoo@gmail.com>
2026-01-29 23:32:45 -0500

    Change to the assembly script

--- jobs/03_flye.sh
+++ jobs/03_flye.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 #SBATCH --job-name=flye
 #SBATCH --time=04:00:00
-#SBATCH --mem=16GB
+#SBATCH --mem=32GB
+#SBATCH --cpus-per-task=8
 #SBATCH --output=/scratch/%u/salmonella/logs/03_%j.out
 #SBATCH --error=/scratch/%u/salmonella/logs/03_%j.err
 
@@ -14,4 +15,4 @@ apptainer exec containers/flye_2.9.6--py311h2de2dd3_0.sif flye \
 --nano-hq filtlong/filteredSRR32410565.fastq.gz \
 --genome-size 5m \
 --asm-coverage 145 \
--t 16
+-t 8
