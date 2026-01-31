0c1ea8609fd9bde1fde0a54e9f75ee371991e0cc
snsaiphoo <sabrinasaiphoo@gmail.com>
2026-01-31 00:41:52 -0500

    second quast script ref vs polished assembly

--- scripts/05_quast1.sh
+++ scripts/05_quast1.sh
@@ -7,4 +7,4 @@ mkdir quast
 module load apptainer
 
 apptainer exec containers/quast_5.3.0--py313pl5321h5ca1c30_2.sif quast.py polishing/consensus.fasta -R
-assembly/assembly.fasta -o quast
+assembly/assembly.fasta -o quast/quast_01
