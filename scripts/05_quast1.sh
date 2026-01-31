4738e69b1e587bbdb021b3ec9361ff3ca5bae54b
snsaiphoo <sabrinasaiphoo@gmail.com>
2026-01-31 00:09:22 -0500

    Adding first quast script

---
+++ scripts/05_quast1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /scratch/$USER/salmonella
+
+mkdir quast
+
+module load apptainer
+
+apptainer exec containers/quast_5.3.0--py313pl5321h5ca1c30_2.sif quast.py polishing/consensus.fasta -R
+assembly/assembly.fasta -o quast
