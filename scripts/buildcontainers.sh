50b3d6506afd3db032010db1901065657fb7b2e3
snsaiphoo <sabrinasaiphoo@gmail.com>
2026-01-31 00:02:12 -0500

    Adding QUAST container

--- scripts/buildcontainers.sh
+++ scripts/buildcontainers.sh
@@ -12,13 +12,13 @@ cd containers
 # Location: scratch/$USER/salmonella/containers
 
 # NanoPlot version 1.46.2
-
 singularity pull docker://quay.io/biocontainers/nanoplot:1.46.2--pyhdfd78af_0
 
 # Flye version 2.9.6
-
 singularity pull docker://quay.io/biocontainers/flye:2.9.6--py311h2de2dd3_0
 
 # Medaka version 2.1.1
-
 singularity pull docker://quay.io/biocontainers/medaka:2.1.1--py311h1d3aea1_0
+
+# QUAST version 5.3.0
+singularity pull docker://quay.io/biocontainers/quast:5.3.0--py313pl5321h5ca1c30_2
